a28219454558cd5bb533bfdce4ea7bf8c1ae3feb
Cristo378 <cristovaohenrique157@gmail.com>
2021-10-11 21:52:54 +0000

    estilo da pagina

--- delete.php
+++ delete.php
@@ -22,15 +22,19 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 <html lang="pt-br">
     <head>
         <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
         <title>Remover Gênero</title>
     </head>
     <body>
-        <h1>Remover Gênero</h1>
-        <p>Tem certeza que deseja excluir o gênero <?= $genero['nome'] ?>?</p>
-
-        <form action="delete.php" method="post">
-            <input type="hidden" name="id" value="<?= $genero['id'] ?>" />
-            <button type="submit">Excluir</button>
-        </form>
+        <main>
+            <p>Tem certeza que quer remover o gênero "<?= $genero['nome'] ?>" ?</p>
+                <form action="delete.php" method="post">
+                    <div class="form-group">
+                        <input class="form-control" type="hidden" name="id" value="<?= $genero['id'] ?>"/>
+                    </div>
+                    <a class="btn btn-secondary" href="index.php">Voltar</a>
+                    <button class="btn btn-danger" type="submit">Excluir</button>
+                </form>
+        </main>
     </body>
 </html>
--- insert.php
+++ insert.php
@@ -15,19 +15,20 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     <head>
         <meta charset="utf-8">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-
-        <title>Document</title>
+        <title>Novo Gênero</title>
     </head>
     <body>
-        <main class="container"
-        <h1>Novo Gênero</h1>
-        <form action="insert.php" method="post">
-            <div class=form-group">
-            <label for="nome">Nome</label>
-            <input class="form-control" type="text" name="nome" />
-            </div>
-            <br />
-            <button class="btn btn-success" type="submit">Salvar</button>
-        </form>
+        <main class="container">
+            <h1>Novo Gênero</h1>
+            <form action="insert.php" method= post>
+                <div class="form-group">
+                    <label for="nome">Nome</label>
+                    <input class="form-control" type="text" name="nome" id="">
+                </div>
+                <br>
+                <a class="btn btn-secondary" href="index.php">Voltar</a>
+                <button class="btn btn-success" type="submit">Salvar</button>
+            </form>
+        </main>
     </body>
 </html>
